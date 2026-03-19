5b422b9308b91db2fcc733e4beda0c48084a0093
goapp <clickandthink1@gmail.com>
2026-03-18 22:42:02 -0300

    update manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/tgo192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/real512.jpg',
